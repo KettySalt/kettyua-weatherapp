956204e5555ed1038d647b1a05fa3eceaf374d89
Ketty <yuriy.sev51@gmail.com>
2022-11-08 00:22:35 +0200

    Created new functions for units switching

--- src/index.js
+++ src/index.js
@@ -57,49 +57,36 @@ function formatDate(timestamp) {
 
 function showDegreeFahrenheit(event) {
   event.preventDefault();
-  let temperature = document.querySelector("#today-temperature");
-  temperature.innerHTML = convertCtoF(celsiusTemperature);
+  let city = document.querySelector("h1");
+  city = city.innerHTML;
+  let units = "imperial";
+  let apiKey = "3te112f50837749e5bfeo6adf636e68f";
+  let currentUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  let forecastWeather = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+
+  axios.get(currentUrl).then(showWeather);
+  axios.get(forecastWeather).then(displayForecast);
+
   let fahrinheitUnit = document.querySelector("#fahrenheit");
   fahrinheitUnit.classList.add("active");
   let celsiusUnit = document.querySelector("#сelsius");
   celsiusUnit.classList.remove("active");
-  let feelings = document.querySelector("#feels-like");
-  feelings.innerHTML = `${convertCtoF(feelingTemperature)}℉`;
-
-  let tempFirst = document.querySelector("#temp-first");
-  let tempSecond = document.querySelector("#temp-second");
-  let tempThird = document.querySelector("#temp-third");
-  let tempFourth = document.querySelector("#temp-fourth");
-  let tempFifth = document.querySelector("#temp-fifth");
-
-  tempFirst.innerHTML = `${convertCtoF(celsiusTemperatureFirst)}℉`;
-  tempSecond.innerHTML = `${convertCtoF(celsiusTemperatureSecond)}℉`;
-  tempThird.innerHTML = `${convertCtoF(celsiusTemperatureThird)}℉`;
-  tempFourth.innerHTML = `${convertCtoF(celsiusTemperatureFourth)}℉`;
-  tempFifth.innerHTML = `${convertCtoF(celsiusTemperatureFifth)}℉`;
 }
 function showDegreeCelsius(event) {
   event.preventDefault();
-  let temperature = document.querySelector("#today-temperature");
-  temperature.innerHTML = celsiusTemperature;
+  let city = document.querySelector("h1");
+  city = city.innerHTML;
+  let units = "metric";
+  let apiKey = "3te112f50837749e5bfeo6adf636e68f";
+  let currentUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  let forecastWeather = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+
+  axios.get(currentUrl).then(showWeather);
+  axios.get(forecastWeather).then(displayForecast);
   let celsiusUnit = document.querySelector("#сelsius");
   celsiusUnit.classList.add("active");
   let fahrinheitUnit = document.querySelector("#fahrenheit");
   fahrinheitUnit.classList.remove("active");
-  let feelings = document.querySelector("#feels-like");
-  feelings.innerHTML = `${feelingTemperature}℃`;
-
-  let tempFirst = document.querySelector("#temp-first");
-  let tempSecond = document.querySelector("#temp-second");
-  let tempThird = document.querySelector("#temp-third");
-  let tempFourth = document.querySelector("#temp-fourth");
-  let tempFifth = document.querySelector("#temp-fifth");
-
-  tempFirst.innerHTML = `${celsiusTemperatureFirst}℃`;
-  tempSecond.innerHTML = `${celsiusTemperatureSecond}℃`;
-  tempThird.innerHTML = `${celsiusTemperatureThird}℃`;
-  tempFourth.innerHTML = `${celsiusTemperatureFourth}℃`;
-  tempFifth.innerHTML = `${celsiusTemperatureFifth}℃`;
 }
 
 function showWeather(response) {
@@ -112,7 +99,7 @@ function showWeather(response) {
 
   let feelsLike = document.querySelector("#feels-like");
   feelingTemperature = Math.round(response.data.temperature.feels_like);
-  feelsLike.innerHTML = `${feelingTemperature}℃`;
+  feelsLike.innerHTML = `${feelingTemperature}º`;
 
   let pressure = document.querySelector("#pressure");
   pressure.innerHTML = `${response.data.temperature.pressure} hPa`;
@@ -145,7 +132,7 @@ function displayForecast(response) {
         forecastHTML +
         `
     <div class="col-2">
-      <div>${formatDate(forecastDay.time * 1000)}</div>
+      <div class="week-days">${formatDate(forecastDay.time * 1000)}</div>
         <img src="${forecastDay.condition.icon_url}" alt="" width="60" />
         <div class="weather-forecast-temperatures">
         <span class="weather-forecast-temperature-max">${Math.round(
@@ -163,58 +150,23 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function showForecast(response) {
-  let firstdayIco = document.querySelector("#first-day");
-  let seconddayIco = document.querySelector("#second-day");
-  let thirddayIco = document.querySelector("#third-day");
-  let fourthIco = document.querySelector("#fourth-day");
-  let fifthdayIco = document.querySelector("#fifth-day");
-  firstdayIco.setAttribute("src", response.data.daily[0].condition.icon_url);
-  seconddayIco.setAttribute("src", response.data.daily[1].condition.icon_url);
-  thirddayIco.setAttribute("src", response.data.daily[2].condition.icon_url);
-  fourthIco.setAttribute("src", response.data.daily[3].condition.icon_url);
-  fifthdayIco.setAttribute("src", response.data.daily[4].condition.icon_url);
-
-  let tempFirst = document.querySelector("#temp-first");
-  let tempSecond = document.querySelector("#temp-second");
-  let tempThird = document.querySelector("#temp-third");
-  let tempFourth = document.querySelector("#temp-fourth");
-  let tempFifth = document.querySelector("#temp-fifth");
-  celsiusTemperatureFirst = Math.round(response.data.daily[0].temperature.day);
-  celsiusTemperatureSecond = Math.round(response.data.daily[1].temperature.day);
-  celsiusTemperatureThird = Math.round(response.data.daily[2].temperature.day);
-  celsiusTemperatureFourth = Math.round(response.data.daily[3].temperature.day);
-  celsiusTemperatureFifth = Math.round(response.data.daily[4].temperature.day);
-
-  tempFirst.innerHTML = `${celsiusTemperatureFirst}℃`;
-  tempSecond.innerHTML = `${celsiusTemperatureSecond}℃`;
-  tempThird.innerHTML = `${celsiusTemperatureThird}℃`;
-  tempFourth.innerHTML = `${celsiusTemperatureFourth}℃`;
-  tempFifth.innerHTML = `${celsiusTemperatureFifth}℃`;
-
-  let dateFirst = document.querySelector("#first-date");
-  let dateSecond = document.querySelector("#second-date");
-  let dateThird = document.querySelector("#third-date");
-  let dateFourth = document.querySelector("#fourth-date");
-  let dateFifth = document.querySelector("#fifth-date");
-  dateFirst.innerHTML = formatDate(response.data.daily[0].time * 1000);
-  dateSecond.innerHTML = formatDate(response.data.daily[1].time * 1000);
-  dateThird.innerHTML = formatDate(response.data.daily[2].time * 1000);
-  dateFourth.innerHTML = formatDate(response.data.daily[3].time * 1000);
-  dateFifth.innerHTML = formatDate(response.data.daily[4].time * 1000);
-}
-
 function search(city) {
   let apiKey = "3te112f50837749e5bfeo6adf636e68f";
   let currentUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  let forecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  let forecastWeather = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(currentUrl).then(showWeather);
-  axios.get(forecast).then(displayForecast);
+  axios.get(forecastWeather).then(displayForecast);
 }
 
 function userCity(event) {
   event.preventDefault();
   let inputCity = document.querySelector("#input-city").value;
+  let celsiusUnit = document.querySelector("#сelsius");
+  let fahrinheitUnit = document.querySelector("#fahrenheit");
+  if (fahrinheitUnit.classList.contains("active")) {
+    fahrinheitUnit.classList.remove("active");
+    celsiusUnit.classList.add("active");
+  }
   search(inputCity);
 }
 
@@ -223,9 +175,9 @@ function urlGeo(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let currentUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
-  let forecast = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+  let forecastWeather = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
   axios.get(currentUrl).then(showWeather);
-  axios.get(forecast).then(displayForecast);
+  axios.get(forecastWeather).then(displayForecast);
 }
 
 function requestGeo(event) {
@@ -233,14 +185,6 @@ function requestGeo(event) {
   let inputCity = document.querySelector("#input-city");
   inputCity.value = null;
 }
-
-let celsiusTemperature = null;
-let feelingTemperature = null;
-let celsiusTemperatureFirst = null;
-let celsiusTemperatureSecond = null;
-let celsiusTemperatureThird = null;
-let celsiusTemperatureFourth = null;
-let celsiusTemperatureFifth = null;
 
 let degreeCelsius = document.querySelector("#сelsius");
 let degreeFahrenheit = document.querySelector("#fahrenheit");
